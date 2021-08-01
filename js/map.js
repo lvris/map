0e7f51fc6038065ed9a02369a3b4c1703cb665a5
lvris <lvris@qq.com>
2021-08-01 20:58:22 +0800

    initial

---
+++ js/map.js
@@ -0,0 +1,25 @@
+var options = {
+    chart: {
+        type: 'bar'                          //指定图表的类型，默认是折线图（line）
+    },
+    title: {
+        text: '我的第一个图表'                 // 标题
+    },
+    xAxis: {
+        categories: ['苹果', '香蕉', '橙子']   // x 轴分类
+    },
+    yAxis: {
+        title: {
+            text: '吃水果个数'                // y 轴标题
+        }
+    },
+    series: [{                              // 数据列
+        name: '小明',                        // 数据列名
+        data: [1, 0, 4]                     // 数据
+    }, {
+        name: '小红',
+        data: [5, 7, 3]
+    }]
+};
+// 图表初始化函数
+var chart = Highcharts.chart('container', options);
